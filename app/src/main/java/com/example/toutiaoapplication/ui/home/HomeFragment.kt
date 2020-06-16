b1069b127f3afb6c20663d1ad461f610c45e577a
k1mlka <disword@pm.me>
2020-06-16 19:10:52 +0800

    移除homeFragment中的text 增加recyclerView组件 增加progressbar

--- app/src/main/java/com/example/toutiaoapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/toutiaoapplication/ui/home/HomeFragment.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.example.toutiaoapplication.R
 
 class HomeFragment : Fragment() {
+
+    lateinit var homeRecyclerView: RecyclerView
+    lateinit var progressBar: ProgressBar
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        progressBar = rootView.findViewById(R.id.home_progress_bar)
+        progressBar.visibility = View.INVISIBLE
+
+
+        return rootView
     }
 }
